46d76137cacd8aa653f134fd8c53f688d578e64c
janzaid khaliq <janzaid.ktk98@gmail.com>
2022-09-08 00:35:10 -0700

    dummy api working on university search

--- src/app/services/uni-service.service.ts
+++ src/app/services/uni-service.service.ts
@@ -6,9 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class UserDataService {
   constructor(private http: HttpClient) {}
-  users(batman: any) {
+  users(rec: any) {
     return this.http.get(
-      'https://fake-movie-database-api.herokuapp.com/api?s=batman' + batman
+      'https://hub.dummyapis.com/singlelist?text=Test&noofRecords=' + rec
     );
   }
 }
